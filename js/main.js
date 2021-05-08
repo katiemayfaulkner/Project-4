b41280a16781ddaf8876525349b49e67ae8cf83f
katiemayfaulkner <katiemayfaulkner@users.noreply.github.com>
2021-05-08 20:25:30 +0200

    feat: Build dropdown menu

---
+++ js/main.js
@@ -0,0 +1,17 @@
+// var dropdownBtn = document.getElementById("dropdown")
+// var dropdownMenu = document.getElementById("dropdown-menu")
+// console.log(dropdownBtn)
+
+// dropdownBtn.onclick = function() {
+//   dropdownMenu.style.display = "flex"   
+// }
+
+
+function toggleFunction() {
+    var dropdownMenu = document.getElementById("dropdown-menu")
+    if (dropdownMenu.style.display = "none") {
+        dropdownMenu.style.display = "flex";
+    } else {
+        dropdownMenu.style.display = "none" 
+    }
+}
