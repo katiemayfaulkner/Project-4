4da2aa9119b17a82d3adbcf09c2e09b281819be9
katiemayfaulkner <katiemayfaulkner@users.noreply.github.com>
2021-05-10 10:28:08 +0200

    a11y: Made buttons more compatible with screen readers

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
-// var dropdownBtn = document.getElementById("dropdown")
-// var dropdownMenu = document.getElementById("dropdown-menu")
-// console.log(dropdownBtn)
+var dropdownBtn = document.getElementById("dropdown")
+var dropdownMenu = document.getElementById("dropdown-menu")
+console.log(dropdownBtn)
 
-// dropdownBtn.onclick = function() {
-//   dropdownMenu.style.display = "flex"   
-// }
-
-
-function toggleFunction() {
-    var dropdownMenu = document.getElementById("dropdown-menu")
-    if (dropdownMenu.style.display = "none") {
-        dropdownMenu.style.display = "flex";
-    } else {
-        dropdownMenu.style.display = "none" 
-    }
+dropdownBtn.onclick = function() {
+  dropdownMenu.style.display = "flex"   
 }
+
+
+// function toggleFunction() {
+//     var dropdownMenu = document.getElementById("dropdown-menu")
+//     if (dropdownMenu.style.display = "none") {
+//         dropdownMenu.style.display = "flex";
+//     } else {
+//         dropdownMenu.style.display = "none" 
+//     }
+// }
