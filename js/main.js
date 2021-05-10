3b9d887a87f2063b87bce92b95f6a99f9131480f
katiemayfaulkner <katiemayfaulkner@users.noreply.github.com>
2021-05-10 23:30:25 +0200

    mentor session

--- js/main.js
+++ js/main.js
@@ -2,16 +2,9 @@ var dropdownBtn = document.getElementById("dropdown")
 var dropdownMenu = document.getElementById("dropdown-menu")
 console.log(dropdownBtn)
 
-dropdownBtn.onclick = function() {
-  dropdownMenu.style.display = "flex"   
-}
-
-
-// function toggleFunction() {
-//     var dropdownMenu = document.getElementById("dropdown-menu")
-//     if (dropdownMenu.style.display = "none") {
-//         dropdownMenu.style.display = "flex";
-//     } else {
-//         dropdownMenu.style.display = "none" 
-//     }
+// dropdownBtn.onclick = function() {
+//   dropdownMenu.style.display = "flex"   
 // }
+dropdownBtn.addEventListener('click', function() {
+  dropdownMenu.classList.toggle('show')
+})
