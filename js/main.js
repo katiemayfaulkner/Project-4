6dfc2b4cabedc8f018f18647af208ebc1623960c
katiemayfaulkner <katiemayfaulkner@users.noreply.github.com>
2021-05-23 12:26:36 +0200

    refactor: fixing html validator errors/warnings

--- js/main.js
+++ js/main.js
@@ -36,7 +36,7 @@ function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("demo");
-  var captionText = document.getElementById("caption");
+  var captionText = document.getElementsByClassName("caption");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
